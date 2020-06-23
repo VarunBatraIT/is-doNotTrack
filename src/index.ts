f628fdd2db4129f9e1790b474387ee85c2fc590c
Varun Batra <codevarun@gmail.com>
2020-06-23 13:10:11 +0530

    Initial Release

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+declare global {
+  interface Navigator {
+    msDoNotTrack: any;
+  }
+  interface External {
+    msTrackingProtectionEnabled: () => boolean;
+  }
+}
+export default class DoNotTrack {
+  public static isEnabled(): boolean | Error {
+    if (
+      window.doNotTrack ||
+      navigator.doNotTrack ||
+      navigator.msDoNotTrack ||
+      'msTrackingProtectionEnabled' in window.external
+    ) {
+      if (
+        window.doNotTrack === '1' ||
+        window.navigator.doNotTrack === 'yes' ||
+        window.navigator.doNotTrack === '1' ||
+        window.navigator.msDoNotTrack === '1'
+      ) {
+        return true;
+      } else if ('msTrackingProtectionEnabled' in window.external && window.external.msTrackingProtectionEnabled()) {
+        // This condition has to be defined seperately to avoid msTrackingProtectionEnabled undefined error in case DoNotTrack is disabled 
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return new Error('Unsupported!');
+    }
+  }
+}
